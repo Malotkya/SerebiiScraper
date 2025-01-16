402392a5eb1f3c1d224dc1d3e8512bc44fd6a99d
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-16 15:51:40 -0600

    Added cache to regionInfo.

--- src/Serebii/Region.ts
+++ src/Serebii/Region.ts
@@ -1,8 +1,9 @@
-import { BASE_UTI, fetchRegionPokedex, convertNameToNumber } from "./index.js";
+import { FileCache } from "../util.js";
+import { fetchRegionPokedex, convertNameToNumber } from "./index.js";
 import Pokemon, {fetchPokemonData} from "./Pokemon.js";
 
 export const REGION_MAP = {
-    "Kanto": ["/pokemon/gen1pokemon.shtml"],
+    "Kanto": ["/pokemon/gen1pokemon.shtml", "/pokedex"],
     "TEST": ""
 } as const;
 
@@ -14,10 +15,15 @@ export const ALL_REGIONS:Region[] = Object.keys(REGION_MAP);
 
 export async function fetchRegionInfo(r:Region):Promise<[Pokemon[], Record<string, string>]> {
     const [pokedex, uriStart] = REGION_MAP[r];
+    const cache = new FileCache();
+
+    if(cache.has(uriStart)) {
+        return JSON.parse(cache.get(uriStart)!)
+    }
     
     const output:Pokemon[] = [];
     const masterMap:Record<string, string> = {};
-    const getUri = (name:string) => `${BASE_UTI+uriStart}/${convertNameToNumber(name)}.shtml`;
+    const getUri = async(name:string) => `${uriStart}/${await convertNameToNumber(name)}.shtml`;
 
     const list = await fetchRegionPokedex(pokedex);
     let count = 0;
@@ -25,7 +31,7 @@ export async function fetchRegionInfo(r:Region):Promise<[Pokemon[], Record<strin
         process.stdout.write(`\u001b[${0}A`);
 
         try {
-            const [value, map] = await fetchPokemonData(getUri(name));
+            const [value, map] = await fetchPokemonData(await getUri(name));
             output.push(value);
             for(name in map){
                 masterMap[name] = map[name];
@@ -36,6 +42,8 @@ export async function fetchRegionInfo(r:Region):Promise<[Pokemon[], Record<strin
 
         console.log(`${Math.ceil((count++ / list.length) * 100)}%`);
     }
+
+    cache.set(uriStart, JSON.stringify([output, masterMap]));
 
     return [output, masterMap];
 }
