1e4e8a2e30b32284037351ac8dbfb40a618511e6
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-16 10:39:36 -0600

    Added FileCache utility class.

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,7 @@
 import nodeFetch from "node-fetch";
 import {JSDOM, DOMWindow} from 'jsdom';
+import fs from "node:fs";
+import path from "node:path";
 
 /** Cache and Queue used by fetch call.
  * 
@@ -57,8 +59,6 @@ export async function fetchDom(url:string):Promise<DOMWindow> {
     return new JSDOM(await fetch(url)).window;
 }
 
-
-
 /** Raw Data
  * 
  * Uses a hash-like to cleanse and estimate the key value
@@ -111,5 +111,42 @@ export class RawData extends Map<string, string> {
         }
 
         console.debug(JSON.stringify(data, null, 2));
+    }
+}
+
+/** File Cache For Storing Large Downloads.
+ * 
+ */
+export class FileCache {
+    private _location:string;
+
+    constructor(location:string = "cache"){
+        this._location = path.join(process.cwd(), location);
+        if(!fs.existsSync(this._location)) {
+            fs.mkdirSync(this._location), {recursive: true};
+        }
+    }
+
+    set(key:string, value:string) {
+        fs.writeFileSync(path.join(this._location, key), value);
+    }
+
+    has(key:string){
+        return fs.existsSync(path.join(this._location, key));
+    }
+
+    get(key:string) {
+        const filename = path.join(key);
+
+        if(fs.existsSync(filename) === false)
+            return null;
+
+        return fs.readFileSync(filename).toString();
+    }
+
+    clear() {
+        for(const key of fs.readdirSync(this._location)) {
+            fs.rmSync(path.join(this._location, key))
+        }
     }
 }
