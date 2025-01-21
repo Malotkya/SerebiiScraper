32a6c2f579ace88b137194a05605d4c2b82beeb3
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-21 13:41:04 -0600

    Used simplify to fix duplicate data errors, and used seperate functions to make easier to read.

--- src/Data/Attack.ts
+++ src/Data/Attack.ts
@@ -2,8 +2,9 @@
  * 
  * Attack Data as Stored in Database.
  */
+import { missingAttacks } from "../fixes.js";
 import Attack, { fetchAttackDataList, ATTACK_GENERATIONS } from "../Serebii/Attack.js"
-import { getLastGen } from "../util.js"
+import { getLastGen, simplify } from "../util.js"
 import PokemonData from "./Pokemon.js";
 /** Attack Data 
  * 
@@ -19,6 +20,41 @@ interface AttackData extends Attack{
 }
 export default AttackData;
 
+export function createNew(data:Attack, generation:number):AttackData {
+    const changes:Record<number, {}> = {};
+    changes[generation] = {};
+
+    return {
+        ...data,
+        changes
+    }
+}
+
+function update(record:AttackData, update:Attack, generation:number): void {
+    const last = getLastGen(record.changes);
+    record.changes[generation] = {};
+
+    if(update.pp !== record.pp) {
+        record.changes[last].pp = record.pp;
+        record.pp = update.pp
+    }
+
+    if(update.power !== record.power) {
+        record.changes[last].power = record.power;
+        record.power = update.power
+    }
+
+    if(update.accuracy !== record.accuracy) {
+        record.changes[last].accuracy = record.accuracy;
+        record.accuracy = update.accuracy
+    }
+
+    if(update.effect !== record.effect) {
+        record.changes[last].effect = record.effect;
+        record.effect = update.effect
+    }
+}
+
 /** Fetch All Attack Data
  * 
  * @returns {Promise<AttackData[]>}
@@ -32,50 +68,17 @@ export async function fetchAllAttackData():Promise<AttackData[]>{
 
         const data = await fetchAttackDataList(ATTACK_GENERATIONS[i]);
         for(const name in data){
+            const id = simplify(name);
 
-            if(masterData[name] === undefined) {
-                const changes:Record<number, {}> = {};
-                changes[generation] = {};
-
-                masterData[name] = {
-                    ...data[name],
-                    changes
-                }
-
+            if(masterData[id] === undefined) {
+                masterData[id] = createNew(data[name], generation);
             } else {
-                const update = data[name];
-                const record = masterData[name];
-
-                const last = getLastGen(record.changes);
-                record.changes[generation] = {};
-
-
-                if(update.pp !== record.pp) {
-                    record.changes[last].pp = record.pp;
-                    record.pp = update.pp
-                }
-
-                
-                if(update.power !== record.power) {
-                    record.changes[last].power = record.power;
-                    record.power = update.power
-                }
-
-                if(update.accuracy !== record.accuracy) {
-                    record.changes[last].accuracy = record.accuracy;
-                    record.accuracy = update.accuracy
-                }
-
-                
-                if(update.effect !== record.effect) {
-                    record.changes[last].effect = record.effect;
-                    record.effect = update.effect
-                }
+                update(masterData[id], data[name], generation)
             }
         }
     }
 
-    return Object.values(masterData);
+    return Object.values(masterData).concat(await missingAttacks());
 }
 
 /** Verify Attack Data
@@ -102,7 +105,18 @@ export function verifyAttackData(data: AttackData[], pokemon: PokemonData[]):boo
         }
     }
 
-    const list:string[] = data.map(a=>a.name).filter(m=>check.has(m));
+    const list:string[] = data.filter(move=>{
+        //Skip Shadow Type Attacks
+        if(move.name.includes("Shadow") && move.type === "???")
+            return false;
+    
+        //Skip Unique Torgue Moves
+        if(!move.name.includes("Torque"))
+            return false;
+            
+        //Skip Struggle
+        return move.name !== "Struggle";
+    }).map(a=>a.name).filter(m=>!check.has(m));
 
     for(const error of list){
         console.debug(error);
