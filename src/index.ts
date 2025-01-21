cccd507fa12661e52bcf7cf0e43c25da70752e49
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-21 14:44:21 -0600

    Updated testing command.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { scrapeData, exportData } from "./Data/index.js";
-import { fetchAllPokemonData } from "./Data/Pokemon.js";
 
-fetchAllPokemonData().then(()=>{
-    console.log("Done!");
-});
+(async()=>{
+    try {
+        console.log("Starting!");
+        const {games, items, pokemon, abilities, moves} = await scrapeData();
+
+        console.log("Exporting Data!");
+        await exportData(games, items, pokemon, abilities, moves);
+
+        console.log("Complete!");
+    } catch (e:any){
+        console.error(e.message || String(e));
+    }
+    
+})()
