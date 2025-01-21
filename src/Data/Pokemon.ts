712d97d4f7326e025fe6608c2fe846f872b673b5
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-21 11:20:56 -0600

    Changed how versions are stored, and using new method for downloading all pokemon.

--- src/Data/Pokemon.ts
+++ src/Data/Pokemon.ts
@@ -4,10 +4,11 @@
  * 
  * @author Alex Malotky
  */
-import Pokemon from "../Serebii/Pokemon.js";
-import { fetchRegionInfo, ALL_REGIONS, REGION_MAP } from "../Serebii/Region.js";
+import { fetchNationalDex } from "../Serebii/index.js";
+import Pokemon, { fetchPokemonGenerations, fetchPokemonData } from "../Serebii/Pokemon.js";
+import { getGenerationByNumber, getGenerationByUri, POKEDEX_GENERATION_LIST } from "../Serebii/Generation.js";
 import Type from "../Serebii/Type.js";
-import { getLastGen, arrayEqual } from "../util.js"
+import { getLastGen, arrayEqual, simplify, FileCache } from "../util.js"
 import AttackData from "./Attack.js";
 import Item from "./Item.js";
 
@@ -15,7 +16,7 @@ interface PokemonData {
     name:string,
     number: number,
     types:Type[],
-    versions: Record<string, string>, 
+    versions: string[], 
     abilities: string[],
     moves: string[],
     changes: Record<number, {
@@ -28,18 +29,21 @@ export default PokemonData;
 /** Create New Pokemon Data
  * 
  * @param {Pokemon} data 
- * @param {string} region 
  * @param {string} generation 
  * @returns {PokemonData}
  */
-function createNew(data:Pokemon, region:string, generation:number):PokemonData {
-    const versions:Record<string, string> = {};
-    versions[region] = data.version || "";
+function createNew(data:Pokemon, generation:number):PokemonData {
+    const versions:string[] = [];
+    if(data.version)
+        versions.push(data.version)
     const changes:Record<number, any> = {};
     changes[generation] = {};
+    
+    const abilities = data.abilities.map(normalizeString);
+    const moves = data.moves.map(normalizeString);
 
     return {
-        ...data, versions, changes
+        ...data, versions, changes, abilities, moves
     }
 }
 
@@ -47,11 +51,14 @@ function createNew(data:Pokemon, region:string, generation:number):PokemonData {
  * 
  * @param {PokemonData} record 
  * @param {Pokemon} data 
- * @param {string} region 
  * @param {number} generation 
  */
-function update(record:PokemonData, data:Pokemon, region:string, generation:number):void {
+function update(record:PokemonData, data:Pokemon, generation:number):void {
     const last = getLastGen(record.changes);
+
+    data.abilities = data.abilities.map(normalizeString);
+    data.moves = data.moves.map(normalizeString);
+
     //Combine from Same Generation
     if(last === generation){
         const moves = new Set(record.moves.concat(data.moves));
@@ -63,9 +70,10 @@ function update(record:PokemonData, data:Pokemon, region:string, generation:numb
     //Create Next Generation
     } else {
         record.changes[generation] = {};
+        
 
         if(data.version){
-            record.versions[region] = data.version;
+            record.versions.push(data.version);
         }
 
         if(!arrayEqual(record.moves, data.moves)){
@@ -87,42 +95,91 @@ function update(record:PokemonData, data:Pokemon, region:string, generation:numb
  * @returns {Promise<[PokemonData[], Record<string, string>]>}
  */
 export async function fetchAllPokemonData():Promise<[PokemonData[], Record<string, string>]>{
-    const AllPokemon:Record<string, PokemonData> = {}
+    const AllPokemon:PokemonData[] = [];
     const AllAbilities:Record<string, string> = {};
-    
-    for(const region of ALL_REGIONS) {
-        const [generation] = REGION_MAP[region];
+    const cache = new FileCache("cache/pokemon");
+    console.log("Downloading All Pokemon!\n");
 
-        console.log(`Downloading ${region}\n`);
+    const list = await fetchNationalDex();
+    for(let i=0; i<list.length; i++){
+        process.stdout.write(`\u001b[${0}A`);
 
-        const [pokemon, abilites] = await fetchRegionInfo(region);
+        if(!cache.has(list[i])){
+            const number = 1+1;
+            const gen = getGenerationByNumber(number);
+            const queue = await fetchPokemonGenerations(
+                `${POKEDEX_GENERATION_LIST[gen]}/${
+                    number<1000? `00${number}`.slice(-3): number
+                }.shtml`
+            );
+            
+            //Start with first
+            const [data, abilities] = await fetchPokemonData(queue.pop()!);
+            const pokemon = createNew(data, gen);
 
-        //Update Natures
-        for(const name in abilites){
-            AllAbilities[name] = abilites[name];
-        }
+            while(queue.length > 0){
+                const uri = queue.pop()!;
+                const gen = getGenerationByUri(uri);
+                const [p, a] = await fetchPokemonData(uri);
+                update(pokemon, p, gen);
 
-        //Update Pokemon
-        for(const name in pokemon){
-            if(AllPokemon[name] === undefined){
-                AllPokemon[name] = createNew(pokemon[name], region, generation);
-            } else {
-                update(AllPokemon[name], pokemon[name], region, generation);
+                for(const name in a){
+                    abilities[name] = a[name];
+                }
             }
-        }
-    }
 
-    return [Object.values(AllPokemon), AllAbilities];
+            //Save Data
+            cache.set(list[i], JSON.stringify([pokemon, abilities]))
+
+            //Update Master Lists
+            for(const name in abilities){
+                AllAbilities[name] = abilities[name];
+            }
+            AllPokemon.push(pokemon);
+
+        //If Cached
+        } else {
+            const [pokemon, abilities] = JSON.parse(cache.get(list[i])!)
+            for(const name in abilities){
+                AllAbilities[name] = abilities[name];
+            }
+            AllPokemon.push(pokemon);
+        }
+        
+         console.log(`${Math.ceil((i / list.length) * 100)}%`);
+    }// END For
+
+    return [AllPokemon, AllAbilities];
 }
 
+/** Normalize String
+ * 
+ * @param {string} value 
+ * @returns {string}
+ */
+function normalizeString(value:string):string {
+    const match = value.match(/^(.*?)(?=<)/);
+    if(match)
+        return match[0].trim();
+
+    return value;
+}
+
+/** Verify Pokemon Data
+ * 
+ * @param {PokmeonData[]} pokemon 
+ * @param {AttackData[]} attacks 
+ * @param {string[]} abilities 
+ * @returns {boolean}
+ */
 export function verifyPokemonData(pokemon:PokemonData[], attacks:AttackData[], abilities:string[]):boolean {
-    const check:Array<string> = attacks.map(a=>a.name);
+    const check:Array<string> = attacks.map(a=>simplify(a.name));
 
     for(let i=0; i<pokemon.length; i++) {
         const p = pokemon[i];
 
         if(p.number !== i+1){
-            console.error("Missing Pokmeon at: " + 1);
+            console.error("Missing Pokmeon at: " + (i+1));
             return false;
         }
 
@@ -134,7 +191,7 @@ export function verifyPokemonData(pokemon:PokemonData[], attacks:AttackData[], a
         }
 
         for(const m of p.moves){
-            if(!check.includes(m)) {
+            if(!check.includes(simplify(m))) {
                 console.error(`Missing Move ${m} on ${p.name}`);
                 return false;
             }
@@ -152,7 +209,7 @@ export function verifyPokemonData(pokemon:PokemonData[], attacks:AttackData[], a
 
             if(p.changes[g].moves){
                 for(const m of p.changes[g].moves){
-                    if(!check.includes(m)) {
+                    if(!check.includes(simplify(m))) {
                         console.error(`Missing Move ${m} at ${g} on ${p.name}`);
                         return false;
                     }
@@ -164,8 +221,13 @@ export function verifyPokemonData(pokemon:PokemonData[], attacks:AttackData[], a
     return true;
 }
 
+/** Generate Pokemon SQL
+ * 
+ * @param {PokemonData} data 
+ * @returns {string}
+ */
 export function generatePokemonSQL(data:PokemonData[]):string {
-    let buffer = `CREATE TABLE Pokemon(
+    const buffer:string[] = [`CREATE TABLE Pokemon(
         number INTEGER PRIMARY KEY,
         name: TEXT,
         types: TEXT,
@@ -173,10 +235,10 @@ export function generatePokemonSQL(data:PokemonData[]):string {
         abilities: TEXT,
         moves: TEXT,
         changes: TEXT
-    );`.replaceAll(/\s+/g, " ") + "\n";
+    );`.replaceAll(/\s+/g, " ")];
 
     for(let p of data){
-        buffer += `INSERT INTO Pokemon Values(
+        buffer.push(`INSERT INTO Pokemon Values(
             ${p.number},
             "${p.name}",
             "${JSON.stringify(p.types)}",
@@ -184,25 +246,30 @@ export function generatePokemonSQL(data:PokemonData[]):string {
             "${JSON.stringify(p.abilities)}",
             "${JSON.stringify(p.moves)}",
             "${JSON.stringify(p.changes)}
-        )`.replaceAll(/\s+/g, " ") + "\n";
+        )`.replaceAll(/\s+/g, " "));
     }
 
-    return buffer;
+    return buffer.join("\n");
 }
 
+/** Generate Abilities SQL
+ * 
+ * @param {Item[]} data 
+ * @returns {string}
+ */
 export function generateAbilitiesSQL(data:Item[]):string {
-    let buffer = `CREATE TABLE Abilities(
+    const buffer = [`CREATE TABLE Abilities(
         id INTEGER PRIMARY KEY,
         name: TEXT,
         value: TEXT
-    );`.replaceAll(/\s+/g, " ") + "\n";
+    );`.replaceAll(/\s+/g, " ")];
 
     for(const item of data){
-        buffer += `INSERT INTO Abilities Values(
+        buffer.push(`INSERT INTO Abilities Values(
             "${item.name}",
             "${item.value}"
-        )`.replaceAll(/\s+/g, " ") + "\n";
+        )`.replaceAll(/\s+/g, " "));
     }
 
-    return buffer;
+    return buffer.join("\n");
 }
