71e419c1b5ba10bee3a8564eb3b4f5e4d80e75cd
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-22 09:48:43 -0600

    Search on simple instead of name.

--- functions/Pokedex/[Gen_Game]/[Name].ts
+++ functions/Pokedex/[Gen_Game]/[Name].ts
@@ -16,7 +16,7 @@ async function getGeneration(value:string, DB:D1Database):Promise<number|null> {
 
 export const onRequestGet: PagesFunction<Env> = async(context) => {
     const gen = await getGeneration(<string>context.params["Gen_Game"], context.env.DB);
-    const input = context.params["Name"];
+    const input = simplify(<string>context.params["Name"]);
 
     if(gen === null)
         return new Response(`Unable to find game '${context.params["Gen_Game"]}'!`, {status: 404});
@@ -24,20 +24,22 @@ export const onRequestGet: PagesFunction<Env> = async(context) => {
     if(gen < 1 || gen > 9)
         return new Response(`Generation '${gen}' out of range!`, {status: 401});
 
-    const record:Pokemon|null = await context.env.DB.prepare("SELECT * FROM Pokemon WHERE name = ?")
+    const record:Pokemon|null = await context.env.DB.prepare("SELECT * FROM Pokemon WHERE simple = ?")
                     .bind(input).first();
 
     if(record === null)
         return new Response(`Unable to find Pokemon '${input}'!`, {status: 404});
 
     const changes:Record<string, Pokemon> = JSON.parse(record["changes"]!);
-    delete record["changes"];
+
+    if(changes[gen] === undefined)
+        return new Response(`'${record["name"]}' does not exist in generation ${gen}!`, {status: 404});
 
     const name     = record["name"];
     const number   = record["number"];
     const versions = JSON.parse(record["versions"]);
     const types    = JSON.parse(record["types"]);
-    const abilities = getUpdate(changes, gen, "abilities") || JSON.parse(record["abilities"]);  //Error Here
+    const abilities = getUpdate(changes, gen, "abilities") || JSON.parse(record["abilities"]);  //TODO: Fix Error Here
     const moves     = getUpdate(changes, gen, "moves")     || JSON.parse(record["moves"]);
 
     return Response.json({name, number, versions, types, abilities, moves});
