0aab0c249934158752e9244f1faa884a82152c6a
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-21 22:53:24 -0600

    All Item API Data.

---
+++ functions/Item.ts
@@ -0,0 +1,13 @@
+export const onRequestGet: PagesFunction<Env> = async(context) => {
+    const {results, error} = await context.env.DB.prepare("SELECT * FROM Items").all();
+
+    if(error)
+        throw error;
+
+    const output:Record<string, string> = {};
+    for(const record of results){
+        output[<string>record["name"]] = <string>record["value"];
+    }
+
+    return Response.json(output);
+}
