5df4e2521bc37014c89fe733165a505e3c66b579
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-01-22 09:56:48 -0600

    Can get pokedex by game.

--- functions/Pokedex/[Gen_Game]/index.ts
+++ functions/Pokedex/[Gen_Game]/index.ts
@@ -2,10 +2,17 @@ import { simplify } from "../../util";
 
 export const onRequestGet: PagesFunction<Env> = async(context) => {
     const gen = Number(context.params["Gen_Game"]);
-    const game = simplify(<string>context.params["Gen_Game"]);
 
     if(isNaN(gen)) {
-        return new Response("Game Pokedex is currently not supported!", {status: 401}); //TODO: Add this to API
+        const game = simplify(<string>context.params["Gen_Game"]);
+
+        const result = await context.env.DB.prepare("SELECT * FROM Pokedex WHERE id = ?")
+                        .bind(game).first();
+        
+        if(result === null)
+            return new Response(`Unable to find game '${context.params["Gen_Game"]}'!`,{status: 404} );
+
+        return Response.json(JSON.parse(<string>result["value"]))
     }
     
     if(gen < 1 || gen > 9)
